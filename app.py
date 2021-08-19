d31312b1506a1039fb636699c441f888099170e2
ninpnin <vainoyrjanainen@icloud.com>
2021-08-19 22:51:24 +0200

    Implement n-gram counting; remove stopwords

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime, json
 import pandas as pd
 import progressbar
@@ -12,22 +13,66 @@ def read_date(msg):
 
     return date
 
-def main():
-    jstr = open("result.json", "r").read()
+def to_df(args):
+    jstr = open("data/" + args.dataset + ".json", "r").read()
     data = json.loads(jstr)
 
     rows = []
+    stopwords = None
 
+    # Read in list of stopwords if requested
+    if args.stopwords is not None:
+        print(args.stopwords)
+        stopwords_str = open("stopwords/" + args.stopwords + ".json", "r").read()
+        stopwords = set(json.loads(stopwords_str))
+
+    # Create dataframe of messages
     for message in progressbar.progressbar(data["messages"]):
         if message["type"] == "message":
             text = message["text"]
-            sender = message["from"]
-            date = read_date(message)
+            if type(text) == str:
+                if args.lowercase:
+                    text = text.lower()
 
-            row = [sender, text, date]
-            rows.append(row)
-    df = pd.DataFrame(rows, columns=["sender", "text", "date"])
+                if stopwords is not None:
+                    text = [wd for wd in text.split() if wd.lower() not in stopwords]
+                    text = " ".join(text)
 
+                sender = message["from"]
+                date = read_date(message)
+
+                row = [sender, text, date]
+                rows.append(row)
+    return pd.DataFrame(rows, columns=["sender", "text", "date"])
+
+def n_grams(df, n=2, threshold=5):
+    grams = {}
+
+    for message in progressbar.progressbar(list(df["text"])):
+        words = message.split()
+        if len(words) >= n:
+            for i in range(len(words)-n):
+                n_gram = words[i:i+n]
+                n_gram = " ".join(n_gram)
+                grams[n_gram] = grams.get(n_gram, 0) + 1
+
+    grams = [(n_gram, occ) for n_gram, occ in grams.items() if occ > threshold]
+    grams = sorted(grams, key= lambda t: t[1])
+    return grams
+
+def main(args):
+    df = to_df(args)
     print(df)
+
+    grams = n_grams(df, n=3)
+    for ngram, count in grams:
+        print(ngram, count)
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, default="results")
+    parser.add_argument("--stopwords", type=str, default=None)
+    parser.add_argument("--lowercase", type=bool, default=False)
+    args = parser.parse_args()
+
+    main(args)
